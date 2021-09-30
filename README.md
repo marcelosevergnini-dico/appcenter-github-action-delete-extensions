eaef0387dbc295b03543ea77e2c19ce639dc209e
Marcelo Severgnini <marcelo.severgnini@diconium.com>
2021-09-30 11:18:21 +0100

    feat: Initial commit, delete action added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# appcenter-github-actions-extensions
+# diconium Cariad Actions
+## AppCenter Extensions - Range Delete Action
+### Missing delete distributions from the Official AppCenter Github Actions
+
+This action iterates an input range for App Center distributions and deletes it
+
+## Inputs
+
+## `from`
+
+**Required** From which release number to delete. Default `"0"`.
+
+## `to`
+
+**Required** to which release number to delete. Default `"0"`.
+
+uses: actions/appcenter-deletion-action@v1.1
+with:
+    from: '1'
+    to: '50'
---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
+
+try {
+    let owner = core.getInput('owner');  // application owner
+    let appName = core.getInput('appName');  // application name
+    let token = core.getInput('token'); // your token that you copied;
+    let from = core.getInput('from'); // from which release number to delete
+    let to = core.getInput('to'); // by which release number to delete
+
+    let url = `https://api.appcenter.ms/v0.1/apps/${owner}/${appName}/releases/`;
+    let requestInfo = {
+        method: "delete",
+        headers: { "X-API-TOKEN": token }
+    };
+
+    async function* asyncGenerator() {
+        var i = from;
+        while (i <= to) {
+          yield i++;
+        }
+      }
+
+    (async function() {
+        for await (let num of asyncGenerator()) {
+            try {
+                await fetch(url + num, requestInfo);
+                console.log(`Release ${num} deleted`)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    })();
+
+} catch (error) {
+  core.setFailed(error.message);
+}
